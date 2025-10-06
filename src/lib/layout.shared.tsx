011427850c66e1436a864e9f399b87e5c21f5b82
The Teleporter <m6253272@gmail.com>
2025-10-06 09:52:04 +0300

    Update layout.shared.tsx; trying major mono display as the logo font. removed default

--- src/lib/layout.shared.tsx
+++ src/lib/layout.shared.tsx
@@ -1,4 +1,10 @@
-import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import { Major_Mono_Display } from 'next/font/google'
+import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared'
+
+const majorMono = Major_Mono_Display({
+  subsets: ['latin'],
+  weight: ['400'],
+})
 
 /**
  * Shared layout configurations
@@ -12,15 +18,10 @@ export function baseOptions(): BaseLayoutProps {
     nav: {
       title: (
         <>
-          <svg
-            width="24"
-            height="24"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-label="Logo"
-          >
-            <circle cx={12} cy={12} r={12} fill="currentColor" />
-          </svg>
-          My App
+          <span className={majorMono.className}>
+             WWW-LEARN
+          </span>
+        </>
         </>
       ),
     },

2c79904fb9a7b2be78d2c44a919054cdb1300a1f
The Teleporter <m6253272@gmail.com>
2025-10-06 04:07:26 +0000

    tweak: changed default font to Geist, changed default theme to Black

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,14 @@
 import '@/app/global.css';
 import { RootProvider } from 'fumadocs-ui/provider/next';
-import { Inter } from 'next/font/google';
+import { Geist } from 'next/font/google';
 
-const inter = Inter({
+const geist = Geist({
   subsets: ['latin'],
 });
 
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
+    <html lang="en" className={geist.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
